1087c780c6084abe4a62b5be45e1afde7a138780
Brian DeRenzi <bderenzi@dimagi.com>
2019-06-20 10:52:18 +0200

    Log messages to the nurse

--- nurse_queue.py
+++ nurse_queue.py
@@ -8,6 +8,7 @@ import logging
 
 from simple_settings import settings
 
+from send import _log_msg
 from util import Singleton
 
 
@@ -84,10 +85,13 @@ class NurseQueue(metaclass=Singleton):
         _, msg_list = self.popleft()
         self.__current_msg_to_nurse = msg_list[0]
         nl = '\n'
+        msg = (f"The following message(s) are from "
+               f"'{self.__current_msg_to_nurse.first_name}' ({self.__current_msg_to_nurse.chat_src})."
+               f"Your reply will be forwarded automatically.\n\n"
+               f"{nl.join([m.msg for m in msg_list])}")
+        _log_msg(msg, 'system', None,
+                 chat_id=settings.NURSE_CHAT_ID)
         context.bot.send_message(
             settings.NURSE_CHAT_ID,
-            f"The following message(s) are from "
-            f"'{self.__current_msg_to_nurse.first_name}' ({self.__current_msg_to_nurse.chat_src})."
-            f"Your reply will be forwarded automatically.\n\n"
-            f"{nl.join([m.msg for m in msg_list])}")
+            msg)
         self.__pending = True
