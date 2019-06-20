671754c1232c25ebd0a52b9f4d3a98406376e033
Brian DeRenzi <bderenzi@dimagi.com>
2019-06-20 12:11:57 +0200

    Save registration state on replies

--- registration.py
+++ registration.py
@@ -13,17 +13,17 @@ CONFIRM_NAME, ASK_NAME, ASK_CHILD_NAME, ASK_CHILD_BIRTHDAY, PHONE_NUMBER, AWW_LI
 
 def cancel(update, context):
     _log_msg(update.message.text, 'user', update, state='registration')
-    send_text_reply('cancel called', update)
+    send_text_reply('cancel called', update, state='registration')
     pass
 
 
 def start(update, context):
     # TODO: Update this once we are ready to launch?
     send_text_reply(
-        'Hello and welcome. My name is Poshan Didi and I am currently under development.', update)
+        'Hello and welcome. My name is Poshan Didi and I am currently under development.', update, state='registration')
 
     send_text_reply(
-        f'Can I call you {update.message.from_user.first_name}?', update)
+        f'Can I call you {update.message.from_user.first_name}?', update, state='registration')
     return CONFIRM_NAME
 
 
@@ -32,7 +32,7 @@ def _save_name(update, context, name):
     # _log_msg(update.message.text, 'user', update, state='registration')
     context.user_data['preferred_name'] = name
     send_text_reply(
-        f'Ok, thank you {name}. Please tell me what I should call your child (just a first name is fine).', update)
+        f'Ok, thank you {name}. Please tell me what I should call your child (just a first name is fine).', update, state='registration')
     return ASK_CHILD_NAME
 
 
@@ -43,11 +43,11 @@ def confirm_name(update, context):
         return _save_name(update, context, update.message.from_user.first_name)
     elif intent == Intent.NO:
         send_text_reply(
-            'Ok, no problem. What would you like me to call you (just a first name is fine)?', update)
+            'Ok, no problem. What would you like me to call you (just a first name is fine)?', update, state='registration')
         return ASK_NAME
 
     send_text_reply(
-        f"I didn't quite get that. Please respond with 'yes' or 'no'. Can I call you {update.message.from_user.first_name}?", update)
+        f"I didn't quite get that. Please respond with 'yes' or 'no'. Can I call you {update.message.from_user.first_name}?", update, state='registration')
     return CONFIRM_NAME
 
 
@@ -62,7 +62,7 @@ def ask_child_name(update, context):
     context.user_data['child_name'] = update.message.text
     send_text_reply(
         f'Ok, thank you {name}--I will refer to your child as {update.message.text}.\n\nAlmost done--just a few more questions. '
-        f'What is the birthday for {update.message.text}? Please enter in this format: YYYY-MM-DD', update)
+        f'What is the birthday for {update.message.text}? Please enter in this format: YYYY-MM-DD', update, state='registration')
     return ASK_CHILD_BIRTHDAY
 
 
@@ -70,7 +70,7 @@ def ask_child_birthday(update, context):
     _log_msg(update.message.text, 'user', update, state='registration')
     context.user_data['child_birthday'] = update.message.text
     send_text_reply(
-        f'Got it. What is your phone number? Please enter it in the following format: +91dddddddddd, where each d is a number', update)
+        f'Got it. What is your phone number? Please enter it in the following format: +91dddddddddd, where each d is a number', update, state='registration')
     return PHONE_NUMBER
 
 
@@ -80,7 +80,7 @@ def phone_number(update, context):
     # (this doesnt actually matter, but is nice)
     context.user_data['phone_number'] = update.message.text.replace(' ', '')
     send_text_reply(
-        f'Great! What is the name of the AWW here?', update)
+        f'Great! What is the name of the AWW here?', update, state='registration')
     return AWW_LIST
 
 
@@ -88,7 +88,7 @@ def ask_awc_code(update, context):
     _log_msg(update.message.text, 'user', update, state='registration')
     context.user_data['aww'] = update.message.text
     send_text_reply(
-        f'Thanks! Last question: what is the AWC code?', update)
+        f'Thanks! Last question: what is the AWC code?', update, state='registration')
     return AWC_CODE
 
 
@@ -109,7 +109,7 @@ def thanks(update, context):
     )
     Database().insert(new_user)
     send_text_reply(
-        "Thank you! You're all registered. Just wait for your first message, which should come at some point in the next 7 days", update)
+        "Thank you! You're all registered. Just wait for your first message, which should come at some point in the next 7 days", update, state='registration')
     return ConversationHandler.END
 
 
